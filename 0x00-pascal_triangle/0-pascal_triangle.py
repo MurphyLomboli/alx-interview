d0254a7482984f66d770b95e81a21112dc0e1130
MurphyLomboli <murphylomboli@outlook.com>
2023-07-28 00:25:57 -0700

    Project Tasks

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,36 @@
+#!/usr/bin/python3
+""" Pascal triangle module
+"""
+
+
 def pascal_triangle(n):
+    """ Generates a pascal triangle
+
+    Args:
+        n (int): the triangle size
+
+    Returns:
+        List: List of lists
+    """
+
     if n <= 0:
         return []
 
+    # initialize the triangle with the start 1
     triangle = [[1]]
-
+    # loop n times to set rows for the triangle
     for i in range(1, n):
+        # always set a new row with init 1
         row = [1]
-        prev_row = triangle[i - 1]
+        # loop through each row to implement the pascal triangle logic
         for j in range(1, i):
-            row.append(prev_row[j - 1] + prev_row[j])
+            # try the logic and continue if there is any index error
+            try:
+                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+            except Exception as e:
+                continue
+                # print(row)
+        # always add 1 at the end of each row before adding to triangle
         row.append(1)
         triangle.append(row)
 
