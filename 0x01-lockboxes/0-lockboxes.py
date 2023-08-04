53ef7c205a57948c197f9ab7c7a009c6e1aae762
MurphyLomboli <murphylomboli@outlook.com>
2023-08-03 19:04:25 -0700

    Project Tasks

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,21 +1,25 @@
-# 0-lockboxes.py
+#!/usr/bin/python3
+"""Lockboxes Contains method that finds the keys to
+open other lockboxes
+"""
+
 
 def canUnlockAll(boxes):
-    if not boxes or not isinstance(boxes, list):
-        raise ValueError("Input should be a non-empty list of lists.")
+    """
+    Function that determines if you can open all the lockboxes
+    Args:
+        boxes: list of lists of integers
+    Returns:
+        True if you can open all the lockboxes, False otherwise
+    """
+    unlocked = set()
 
-    num_boxes = len(boxes)
-    visited = [False] * num_boxes
-    visited[0] = True
-    stack = [0]
-
-    while stack:
-        current_box = stack.pop()
-
-        for key in boxes[current_box]:
-            if 0 <= key < num_boxes and not visited[key]:
-                visited[key] = True
-                stack.append(key)
-
-    return all(visited)
-
+    for box_id, box in enumerate(boxes):
+        if len(box) == 0 or box_id == 0:
+            unlocked.add(box_id)
+        for key in box:
+            if key < len(boxes) and key != box_id:
+                unlocked.add(key)
+        if len(unlocked) == len(boxes):
+            return True
+    return False
